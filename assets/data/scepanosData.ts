f9e7f847f3ebb998e887bdae6c368687cc0be455
serg-sheridan <52678820+serg-sheridan@users.noreply.github.com>
2019-08-08 04:41:05 -0400

    Add files via upload

---
+++ assets/data/scepanosData.ts
@@ -0,0 +1,11 @@
+import { MyProfile } from '../../app/scepanos';
+
+
+export const MYPROFILE : MyProfile = {
+    sNumber : 991141264,
+    sName : 'Srdjan Scepanovic',
+    sLogin : 'scepanos',
+    sCampus : 'Trafalgar',
+    sTitle : 'scepanos-A4'
+};
+
